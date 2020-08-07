cd504f12bef8239d1856128c1f726be73aeb3d7d
Serhii Popov <popow.serhii@gmail.com>
2020-08-07 07:06:46 +0300

    Fix file downloading

--- app/code/local/Etatva/Files/controllers/IndexController.php
+++ app/code/local/Etatva/Files/controllers/IndexController.php
@@ -12,10 +12,10 @@ class Etatva_Files_IndexController extends Mage_Core_Controller_Front_Action
 		$sample = Mage::getModel('files/files')->load($sampleId, 'file_timestamp');
 		if ($sample->getId())
 		{
-            $resource = Mage::getBaseDir('media').DS.'downloadable'.DS.'files'.DS.'links'. $sample->getProdFile();
-             $resourceType = 'file';
+            $resource = Mage::getBaseDir('media') . '/downloadable/files/links' . $sample->getProdFile();
+            $resourceType = 'file';
             try {
-                $this->_processDownload($sample,$resource, $resourceType);
+                $this->_processDownload($sample, $resource, $resourceType);
 				$downloadCount=$sample->getFileDownloads();
                 Mage::getModel('files/files')->load($sample->getId())
 								->setFileDownloads(++$downloadCount)
@@ -26,22 +26,27 @@ class Etatva_Files_IndexController extends Mage_Core_Controller_Front_Action
             }
         }
 	}
-	protected function _processDownload($sample,$resource, $resourceType)
+
+    protected function _processDownload($sample, $resource, $resourceType)
     {
-        $contentDisposition='inline';
-        $fileName=$sample->getProdFile();
-        $charPosition= strrpos($fileName, "/");
-		$fileName=substr($fileName,$charPosition+1);
+        $contentDisposition = 'inline';
+        $fileName = $sample->getProdFile();
+        $charPosition = strrpos($fileName, '/');
+        $fileName = substr($fileName, $charPosition + 1);
+
         if (function_exists('mime_content_type')) {
-                $contentType = mime_content_type(Mage::getBaseDir('media'). 'downloadable'  . 'files' . 'links' .  $sample->getProdFile());
-            } else {
-                $contentType=$sample->getFileType();
-            }
-		$this->getResponse()
+            $contentType = mime_content_type($resource);
+        } else {
+            $contentType = $sample->getFileType();
+        }
+
+        $this->getResponse()
             ->setHttpResponseCode(200)
             ->setHeader('Pragma', 'public', true)
+            ->setHeader('Accept-Ranges', 'bytes', true)
             ->setHeader('Cache-Control', 'must-revalidate, post-check=0, pre-check=0', true)
-            ->setHeader('Content-type', $contentType, true)
+            ->setHeader('Content-Type', $contentType, true)
+            ->setHeader('Content-Transfer-Encoding', 'binary', true)
             ->setHeader('Content-Length', $sample->getFileSize())
             ->setHeader('Content-Disposition', $contentDisposition . '; filename='.$fileName);
 
@@ -50,7 +55,7 @@ class Etatva_Files_IndexController extends Mage_Core_Controller_Front_Action
         $this->getResponse()
             ->sendHeaders();
 
-		$handle = new Varien_Io_File();
+        $handle = new Varien_Io_File();
         $handle->open(array('path'=>Mage::getBaseDir('var')));
         if (!$handle->fileExists($resource, true)) {
             Mage::throwException(Mage::helper('downloadable')->__('The file does not exist.'));
